4b0804c1b4e11914f7bd6df773ef1965b4febac8
viperofficailll <sharadhapokharel777@gmal.com>
2025-06-25 19:18:46 +0545

    nav fixed

--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -1,52 +1,21 @@
-// import ThreeBars from "../Icons/ThreeBars";
+//320 56   //304 40 //156 40 //156 21  //12 19.1
+import ThreeBars from '../Icons/ThreeBars';
+import cmclogo from '../images/cmclogo.jpg'
 
-//320 56   //304 40 //156 40 //156 21
+
 const Navbar = () => {
   return (
-    // <>
-    //   <div
-    //     id="navwrapper"
-    //     className=" bg-[#ffff] w-[320px] h-[56px] p-[8px] flex-col flex box-border justify-center items-center"
-    //   >
-    //     <div
-    //       id="second"
-    //       className=" bg-[#ffff] w-[304px] h-[40px] flex  justify-items-center box-border justify-between items-center"
-    //     >
-    //       <div
-    //         id="Logo"
-    //         className=" w-[156px] h-[40px] items-center flex box-border "
-    //       >
-    //         <h1
-    //           id="logoxa"
-    //           className="  font-bold text-xl text-[#1868db] items-center text-center"
-    //         >
-
-    //           ATLASSIAN
-    //         </h1>
-            
-    //       </div>
-    //       <div id="gapwala" className=" w-[] h-[40px]"></div>
-    //       <div id="bar" className=" w-[21px] h-[40px] box-border items-center">
-    //         <div
-    //           id="3bars"
-    //           className=" w-[21px] h-[20px] box-border justify-center items-center"
-    //         >
-    //           {<ThreeBars></ThreeBars>}
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </>
-    <div className="w-full h-full flex justify-center items-center">
-      <div className="w-full h-14 bg-amber-950 flex justify-between items-center px-5">
-        <div className="w-fit h-fit">
-          <h1>Sandesh</h1>
-        </div>
-        <div>
-          <p>logo</p>
+    <>
+      <div className="w-full h-[8vh]  px-4">
+        <div className=" w-full h-full p-2 flex ">
+          <div className="w-full h-full flex box-border items-center ">
+            <img src={cmclogo} alt="logo" className="w-[12vw] h-full  sm:w-[8vw] lg:w-[6vw]"></img>
+            <h1 className=" font-bold text-[#1868DB] mx-4"> ATLASSIAN</h1>
+          </div>
+          <div className="w-6 h-5 items-center justify-center"><ThreeBars></ThreeBars></div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- Frontend/src/Pages/First.tsx
+++ Frontend/src/Pages/First.tsx
@@ -5,24 +5,7 @@ const First = () => {
   return (
     <div>
       <Navbar></Navbar>
-      {/* <div id="text" className=" bg-emerald-400 w-[320px] h-[327px]">
-        <h1
-          id="text"
-          className=" sm bg-yellow-400 w-[401px] h-full box-border font-normal text-[#101214] leading-[60px] text-[50px] flex flex-col justify-center text-center "
-        >
-          {" "}
-          The new Jira:{" "}
-            <div>from
-            <span className="font-extrabold text-[48px] border-box">
-             {""} teams{" "}
-            </span>to</div>
-          dreams
-        </h1>
-        <div id="getstarted" className=" bg-red-400 w-[320px] h-[180px]">
-          {" "}
-          <a className="">Get Started</a>
-        </div>
-      </div> */}
+   <div className="w-full h-[] bg-red-900" ></div>
     </div>
   );
 }
